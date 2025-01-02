64d5be7c9f47c2de024a69cdf977b02639ec8a87
soham <sohamkgaikwad@gmail.com>
2025-01-02 12:59:11 +0530

    playing with routes a little

--- src/app.js
+++ src/app.js
@@ -1,17 +1,35 @@
 const express = require("express");
 const app = express();
 
-app.use("/help",(req,resp)=>
-{
-    resp.send("Help Section");
-});
-
-app.use('/',(req,resp)=>{
-    resp.send("Home page")
-});
 
 app.listen(6969,()=>
 {
     console.log("Server is Running");
 });
+
+// app.use("/",(req,resp)=>{
+//     resp.send("Home page")
+// });
+
+app.get("/test/:userId",(req,res)=>
+{
+    console.log(req.params);
+    res.send({firstName : "Soham", lastName : "Gaikwad"})});
+    
+app.get("/test",(req,res)=>
+{
+    console.log(req.query);
+    res.send({firstName : "Soham", lastName : "Gaikwad"})});
+
+app.post("/test",(req,res) =>
+{
+    console.log("Used Post Method");
+    res.send("Data Sent Successfully to the server");
+})
+
+app.delete("/test",(req,res)=>
+{
+    console.log("Delete Executed");
+    res.send("Data Deleted Successfully")
+})
 
