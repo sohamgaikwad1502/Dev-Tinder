7ff2498e51ead176f11b99471c4e6e62f3683c19
soham <sohamkgaikwad@gmail.com>
2025-01-05 23:51:22 +0530

    added validations using validator liabrary

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const validation = require("validator");
 const {connectDb} = require("./config/database.js");
 const User = require("./models/user.js")
 const app = express();
@@ -23,13 +24,121 @@ app.post("/signup",async (req,res) =>
     // new instace of new user model
     console.log(req.body);
     const user = new User(req.body);
+    const emailId = req.body.emailId;
     try{
+        // if (!validation.isEmail(emailId))
+        // {
+        //     throw new Error("Email is not valid");
+        // }
         await user.save(); 
         res.send("Data Saved Successfully!!");
     }
     catch(err)
     {
-        res.status(401).send("Error Saving the User data" + err.message )
+        res.send( err.message)
     }
     
+})
+
+app.get("/user", async (req,res) =>
+{
+    const email =  req.body.emailId;
+    try {
+        const userfromdb = await User.find({emailId : email});
+        (userfromdb.length === 0 ) ? res.status(404).send("User Not Found") : res.send(userfromdb) ;
+    }
+    catch(error) 
+    {
+        res.status(404).send("Record Not Found" + error)
+    }
+    
+})
+
+app.get("/feed",async(req,res) =>
+{
+    try
+    {
+        const alldata = await User.find({});
+        res.send(alldata);
+    }
+    catch(err)
+    {
+        res.status(404).send("No data Found");
+    }
+})
+
+app.get("/getone" , async (req, res) =>
+{
+    const emailtofind = req.body.emailId;
+    try
+    {
+        const document = await User.findOne({emailId : emailtofind});
+        (document) ? res.send(document) : res.send("No Data Found");
+        
+    }
+    catch(error)
+    {
+        res.status(404).send("Error Occured" + error);
+    }
+})
+
+//Delete api 
+
+app.delete("/delete", async (req,res) =>
+{
+    const userId = req.body.userId;
+    try {
+        await User.findByIdAndDelete(userId);
+        res.send("Data Deleted Successfully");
+    }
+    catch(err)
+    {
+        res.status(404).send("Error Deleting Data" + err);
+    } 
+})
+
+//Update API
+app.patch("/update", async (req,res) => 
+{
+    const emailId = req.body.emailId;
+    const updateData = req.body;
+    
+    try 
+    {
+        if (!validation.isEmail(emailId))
+        {
+            throw new Error("Email is not valid");
+        }
+        await User.findOneAndUpdate({emailId},updateData,{runValidators: true});
+        res.send("Data Updated Successfully");
+    }
+    catch(err)
+    {
+        res.status(404).send(err.message)
+    }
+})
+
+app.patch("/updateBasedOnId/:userId" , async(req,res) =>
+{
+    const userId = req.params?.userId;
+    const data = req.body;
+    
+
+    try 
+    {
+        //Api level Validations
+        const ALLOWED_CHANGES = ["photoUrl","about","gender","age","skills"]
+        const isUpdateAllowed =  Object.keys(data).every(k => ALLOWED_CHANGES.includes(k))
+
+        if(!isUpdateAllowed)
+        {
+            throw new Error("Update Not Allowed");
+        }
+        await User.findByIdAndUpdate(userId,data,{runValidators : true});
+        res.send("Data Updated Successfully")
+    }
+    catch(err)
+    {
+        res.status(404).send(err.message);
+    }
 })
--- src/models/user.js
+++ src/models/user.js
@@ -1,31 +1,94 @@
 const mongoose = require('mongoose');
-
+const validation = require("validator");
 //Creating schema for DB
 const userSchema = mongoose.Schema({
     firstName : {
-        type:String
+        type:String,
+        required:true,
+        minlength:2 
     },
     lastName : 
     {
-        type:String
+        type:String,
+        required : true
     },
     emailId :
     {
-        type:String
+        type:String,
+        required : true,
+        unique : true,
+        lowercase : true,
+        trim : true,
+        validate : function validate(value)
+        {
+            if(!validation.isEmail(value))
+            {
+                throw new Error("Email is not valid")
+            }
+        }
     },
     password :
     {
-        type:String
+        type:String,
+        required: true,
+        minlength: 8,
+        maxLength: 64,
+        validate(value)
+        {
+            if (!validation.isStrongPassword(value))
+            {
+                throw new Error("Password is must contain 1 uppercase, 1 lowercase, 1 special character, 1 number and minimum 8 characters");
+            }
+        }
+
     },
     age: 
     {
-        type:Number
+        type:Number,
+        min: 18,
     },
     gender : 
     {
-        type:String
+        type:String,
+        lowercase : true,
+        validate(value)
+        {
+            if(!["male","female","others"].includes(value))
+            {
+                throw new Error("Gender data is Not valid")
+            }
+        }
+        
+    },
+    photoUrl:  
+    {
+        type : String,
+        default : "https://tinyurl.com/mu7aeawn",
+        validate(value)
+        {
+            if(!validation.isURL(value))
+            {
+                throw new Error("Photo URL is not valid")
+            }
+        }
+    },
+    about : 
+    {
+        type : String,
+        default : "This is the default about of the User"
+    },
+    skills : 
+    {
+        type: [String],
+        validate(value)
+        {
+            if(value.length > 10)
+            {
+                throw new Error("Skills should be at max 10")
+            }
+        }
     }
-})
+} , {timestamps : true})
 
 //Creating user Collection model (Table in terms of SQL) to store user data
 const User = mongoose.model("User", userSchema)
