f453391c6aa6f20651c0dd9931cdec41feb53aa2
Soham Gaikwad <sohamkgaikwad@gmail.com>
2025-04-30 11:10:56 +0530

    added frontend cors url at .env

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ require("./utils/cronJobs");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT,
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   })
